0fbe9bc15d06afeb4f2272be2c73fb5dd4927f9f
KID <50666207+KID421@users.noreply.github.com>
2024-03-24 16:21:08 +0800

    Update 完成生成

--- Assets/Scripts/BasketballSystem.cs
+++ Assets/Scripts/BasketballSystem.cs
@@ -1,7 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BasketballSystem : MonoBehaviour
 {
     [SerializeField]
     private GameObject basketball;
+
+    // Unity 的按鈕跟程式溝通的方式
+    // 需要一個公開的方法，或稱為函式、函數
+    // 方法的語法：
+    // 公開 void 方法名稱 () { 程式內容 }
+    public void SpawnBasketball()
+    {
+        // 生成(物件，座標，角度)；
+        // 此物件的座標 transform.posiotion
+        // 零度角 Quaternion.identity
+        Instantiate(basketball, transform.position, Quaternion.identity);
+    }
 }
