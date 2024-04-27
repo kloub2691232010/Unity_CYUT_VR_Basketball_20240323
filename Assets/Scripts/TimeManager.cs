2a641a99b00d0e414dfb6b2577f90a32d0c0b5b6
kloub2691232010 <h0147xyb@gmail.com>
2024-04-27 13:32:13 +0800

    updata

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField, Header("時間文字")]
+    private TMP_Text textTime;
+    [SerializeField, Header("倒數時間"), Range(10, 100)]
+    private float timeTotal = 60;
+
+    private void Update()
+    {
+        CountDown();
+    }
+
+    private void CountDown()
+    {
+        timeTotal -= Time.deltaTime;
+
+        textTime.text = $"Time - {timeTotal}";
+    }
+}
---
+++ Assets/Scripts/scoremanager.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class scoremanager : MonoBehaviour
+{
+    [SerializeField, Header("分數")]
+    private TMP_Text textScore;
+    
+    private string basketballName = "籃球";
+    private int score;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+       //print(other.name);
+
+       //如果 進入的物件名稱 包含 籃球
+       //就得兩分
+       if (other.name.Contains(basketballName))
+        {
+            score += 2;
+            //print($"<color=#f93>分數:{score}</color>");
+            textScore.text = $"SCORE - {score}";  
+        }
+    }
+}
